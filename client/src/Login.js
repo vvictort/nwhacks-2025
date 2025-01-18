70960af95cc5c0aa8557ffada79265f22cdd16ef
realBarry123 <barryyu28@gmail.com>
2025-01-18 15:10:39 -0800

    Setup routes

---
+++ client/src/Login.js
@@ -0,0 +1,10 @@
+const Login = () => {
+    console.log("login page");
+    return (
+        <div className="login">
+            <h2>This is Login</h2>
+        </div>
+    );
+}
+
+export default Login;
