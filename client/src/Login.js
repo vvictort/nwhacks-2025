0e9d8390787543cb64a9ad7f27a6bd23fcf5f0f4
Victor <vvictort20@gmail.com>
2025-01-19 03:08:19 -0800

    Log-in page styling is done

--- client/src/Login.js
+++ client/src/Login.js
@@ -5,6 +5,7 @@ import './Login.css';
 const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [rememberMe, setRememberMe] = useState(false);
     const navigate = useNavigate();
 
     const handleLogin = (e) => {
@@ -16,33 +17,47 @@ const Login = () => {
     }
 
     return (
-        <div className="login" align="center">
-            <div>
-                <form>
-                    <h2>User Login</h2>
+        <div className="login-sign" align="center" style={{ display: "flex", justifyContent: "center" }}>
+            <div className="shape shape1"></div>
+            <div className="shape shape2"></div>
+            <div className="shape shape3"></div>
+            <div className="shape shape4"></div>
+            <form className="login-form">
+                <div className="welcome">
+                    <h1>[Logo] monenify</h1><br />
+                    <h2>Welcome to monenify! </h2>
+                    <h4>Please sign-in to your account and save money today.</h4><br />
+                </div>
 
-                    <label htmlFor="username">Username</label>
-                    <input required type="text" value={username} placeholder="Username" onChange={(e) => setUsername(e.target.value)} /><br />
+                <label htmlFor="username">Email or Username</label>
+                <input required type="text" value={username} placeholder="Enter your email or username" onChange={(e) => setUsername(e.target.value)}></input>
 
-                    <label htmlFor="password">Password</label>
-                    <input required type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} /><br />
-                    <button classname="login-button" onClick={handleLogin}>Login</button>
+                <label htmlFor="password">Password</label>
+                <input required type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)}></input>
 
-                    <div class="social">
-                        <div class="instagram">
-                            <i class="fab fa-instagram"></i>
-                        </div>
+                <div className="remember-me">
+                    <input type="checkbox" id="rememberMe" checked={rememberMe} onChange={(e) => setRememberMe(e.target.checked)} />
+                    <label htmlFor="rememberMe">Remember me</label>
+                </div>
 
-                        <div class="facebook">
-                            <i class="fab fa-google"></i>
-                        </div>
+                <button className="login-button" onClick={handleLogin}>Login</button>
+
+                <div className="separator">
+                    <span>or</span>
+                </div>
+
+                <div class="social">
+                    <div class="instagram">
+                        <i class="fab fa-instagram"></i>
                     </div>
 
-                </form>
-            </div>
+                    <div class="facebook">
+                        <i class="fab fa-google"></i>
+                    </div>
+                </div>
 
-
-        </div>
+            </form >
+        </div >
     );
 }
 
